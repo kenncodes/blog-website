02e3ffb1d9d4d4f3ca6774a631c129f72704cf7b
kj35 <olivewhitdev@gmail.com>
2022-06-09 23:31:20 -0400

    new react project

--- memories/client/src/App.js
+++ memories/client/src/App.js
@@ -9,33 +9,34 @@ import useStyles from './styles';
 import memories from './images/memories.png';
 
 const App = () => {
+  const [currentId, setCurrentId] = useState(0);
+  const dispatch = useDispatch();
+  const classes = useStyles();
 
-    const classes = useStyles();
-    const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [currentId, dispatch]);
 
-    useEffect(() => {
-        dispatch(getPosts());
-    }, [dispatch])
-    return (
-        <Container maxWidth="lg">
-            <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
-                <img className={classes.image} src={memories} alt="memories" height="60" />
-            </AppBar>
-            <Grow in>
-                <Container>
-                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts />
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Form />
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Grow>
+  return (
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+        <img className={classes.image} src={memories} alt="icon" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts setCurrentId={setCurrentId} />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form currentId={currentId} setCurrentId={setCurrentId} />
+            </Grid>
+          </Grid>
         </Container>
-    )
-}
+      </Grow>
+    </Container>
+  );
+};
 
 export default App;
--- memories/server/index.js
+++ memories/server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 //mongodb
-const CONNECTION_URL = "mongodb+srv://user123:user123@cluster0.n5wzc.mongodb.net/?retryWrites=true&w=majority";
+const CONNECTION_URL = "mongodb+srv://user123:user@cluster0.n5wzc.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL)
