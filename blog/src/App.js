930f3a0295eb5a18f0bd1a31c729bc0208abe51a
kj35 <olivewhitdev@gmail.com>
2022-06-11 23:59:01 -0400

    restart blog with react

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,10 +1,12 @@
-
+import TopBar from './topbar/TopBar'
 
 function App() {
   return (
-    <div className="App">
-     Blog App
-    </div>
+    <>
+        <TopBar/>
+    <p>test</p>
+    </>
+
   );
 }
 
---
+++ blog/src/topbar/TopBar.jsx
@@ -0,0 +1,9 @@
+import './topbar.css'
+
+export default function TopBar(){
+    return (
+        <div className="top">
+            <h1>TopBar</h1>
+        </div>
+    )
+}
