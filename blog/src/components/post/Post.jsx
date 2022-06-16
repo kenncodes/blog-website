ba9b8574a1ac6ae9b47628268fb09af0015dc57e
kj35 <olivewhitdev@gmail.com>
2022-06-15 23:00:28 -0400

    add pages

--- blog/src/components/post/Post.jsx
+++ blog/src/components/post/Post.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./post.css";
 
 export default function Post({img}) {
@@ -11,19 +12,20 @@ export default function Post({img}) {
       <div className="postInfo">
         <div className="postCats">
           <span className="postCat">
+            <Link className="link" to="/posts?cat=Music">
               Music
-   
+            </Link>
           </span>
           <span className="postCat">
-
+            <Link className="link" to="/posts?cat=Music">
               Life
-   
+            </Link>
           </span>
         </div>
         <span className="postTitle">
-          
+          <Link to="/post/abc" className="link">
             Lorem ipsum dolor sit amet
-    
+          </Link>
         </span>
         <hr />
         <span className="postDate">1 hour ago</span>
